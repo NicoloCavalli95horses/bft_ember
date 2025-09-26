7c8eb95626f6861938ba9fa46123e73ce245bc52
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2025-09-26 15:42:05 +0200

    test debug render tree

--- config/environment.js
+++ config/environment.js
@@ -12,7 +12,7 @@ module.exports = function (environment) {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
-      _wefwefwe: false,
+      _DEBUG_RENDER_TREE: false,
     },
 
     APP: {
